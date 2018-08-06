48f95f142e37aa9a973acca98b77427e6b99523f
Lev# <36667906+Lev001@users.noreply.github.com>
2018-08-06 07:50:47 +0400

    first colors

---
+++ ColorsGame/Colors/Assets/scripts/CubeFloat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFloat : MonoBehaviour
+{
+
+    public float speed,tilt;
+    private Vector3 target = new Vector3(0, 1.50f, 0);
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        if (transform.position == target && target.y != -0.5f)
+            target.y = -0.5f;
+        else if (transform.position == target && target.y == -0.5f)
+            target.y = 1.50f;
+        transform.Rotate(Vector3.up * Time.deltaTime * tilt);
+    }
+}
---
+++ ColorsGame/Colors/Assets/scripts/MaxScore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MaxScore : MonoBehaviour {
+
+
+	void Start () {
+        GetComponent<Text>().text = PlayerPrefs.GetInt("Score").ToString();
+	}
+
+}
